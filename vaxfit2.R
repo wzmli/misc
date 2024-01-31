86b5a8d221d9fab29731fa4a4680b7997aae2141
wzmli <wzmichael.li@gmail.com>
2024-01-31 13:25:17 -0600

    Autocommit

--- vaxfit2.R
+++ vaxfit2.R
@@ -4,28 +4,39 @@ library(shellpipes)
 
 dat <- rdsRead()
 
+### BMB's method, create the interaction of the different combinations
+
 dat2 <- (dat
-    |> mutate(vax_type.lag = interaction(vax_type, lag, drop = TRUE))
-	 |> group_by(vax_type.lag)
-	 |> mutate(wavg = weighted.mean(prop,w=size))
+	|> mutate(vax_type.lag = interaction(vax_type, lag, drop = TRUE))
+	|> group_by(vax_type.lag)
+	|> mutate(wavg = weighted.mean(prop,w=size))
 )
 
 print(dat2)
 
 mod2 <- glm(prop ~ month + vax_type.lag
-          , weights = size
-          , data=dat2
-          , family = binomial()
+	, weights = size
+	, data=dat2
+	, family = binomial()
 )
 coef(mod2)    
 
 
 ## vignette("comparisons", package = "emmeans")
 
+## Transforms back to the probability scale?!? when type = response
+
 emm1 <- emmeans(mod2, ~ vax_type.lag,type="response")
 gg <- plot(emm1, type = "response")
 
+print(gg 
+	+ ggtitle("Effects") 
+	+ geom_point(data=dat2,aes(y=vax_type.lag,x=wavg),color="red")
+)
+
 print(confint(emm1))
+
+## Create the contrast to test things or grouping effects?!?
 
 print(contrast(emm1))
 
@@ -41,3 +52,7 @@ cc <- contrast(emm1
 print(cc)
 
 print(confint(cc))
+
+print(plot(cc)+ggtitle("contrast"))
+
+
