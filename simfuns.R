56927379f48974182daa03d477b572cdc0580869
Jonathan Dushoff <dushoff@mcmaster.ca>
2024-02-14 16:36:18 -0500

    alternative hazard code

--- simfuns.R
+++ simfuns.R
@@ -2,11 +2,15 @@ library(shellpipes)
 
 loadEnvironments()
 
+
 vec_sims <- function(pop,vax_vec, vp_vec, ve_vec, foi){
+	f = foi*c(1,1-ve_vec)
 	dd <- data.frame(vax=c("unvax",vax_vec)
 		, size = pop*c(1-sum(vp_vec),vp_vec)
-		, prop = foi*c(1,1-ve_vec)
+		## , prop = f
+		, prop = 1-exp(-f)
 	)
 }
+
 
 saveEnvironment()
